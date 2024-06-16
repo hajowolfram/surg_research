28b9c7210261741fd82318f09a57493e4dbbd9e9
Hajo Wolfram <115335333+hajowolfram@users.noreply.github.com>
2024-06-16 16:19:32 +0900

    Update nba_api_practice.py

--- src/nba_api_practice.py
+++ src/nba_api_practice.py
@@ -1,1 +1,17 @@
+from nba_api.stats.endpoints import playercareerstats
+
+### for familiarising myself with the nba_api
+
+# Nikola Jokic
+career = playercareerstats.PlayerCareerStats(player_id='203999') 
+
+# pandas data frames (optional: pip install pandas)
+jokic_pd = career.get_data_frames()[0]
+# jokic_pd.head()
+
+# json
+career.get_json()
+
+# dictionary
+career.get_dict()
 
