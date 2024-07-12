63f509026dbf547be4d5ebedecb0f20e38a2b238
Ryan Lee <ryanlee@macbook-air-390.lan>
2024-07-12 11:34:38 -0400

    Add restoring force game simulation function

---
+++ src/restoring_force_game_simulation.R
@@ -0,0 +1,129 @@
+# Script contains function for simulating games with restoring force
+
+# Load necessary data from original huang study; file paths may differ
+load('ctmc/nofouls/team_subMC.RData')
+load('ctmc/nofouls/team_pL.RData')
+load('ctmc/nofouls/team_lineups.RData')
+load('ctmc/nofouls/team_lineup_times.RData')
+load('ctmc/nofouls/team_lineup_starting.RData')
+load('ctmc/nofouls/team_lineup_coef.RData') # this data object contains the plus/minus scoring rates for each five-person lineup. 
+
+
+#Simulate matchup
+simulate_matchup <- function(team_a, team_b) {
+  #Running tally of plus/minus
+  a_plus_minus = 0
+  b_plus_minus = 0
+  
+  #Extracting team specific information
+  a_tMC = team_subMC[[team_a]]
+  a_tLU = team_lineups[[team_a]]
+  a_tLT = team_lineup_times[[team_a]]
+  a_tLS = team_lineup_starting[[team_a]]
+  a_tPL = team_pL[[team_a]][,2]
+  a_LU_sim = rep(0, dim(a_tMC)[1])
+  
+  b_tMC = team_subMC[[team_b]]
+  b_tLU = team_lineups[[team_b]]
+  b_tLT = team_lineup_times[[team_b]]
+  b_tLS = team_lineup_starting[[team_b]]
+  b_tPL = team_pL[[team_b]][,2]
+  b_LU_sim = rep(0, dim(b_tMC)[1])
+  
+  # Sampling starting states based on empirical distributions (tLS s) 
+  a_state = sample(1:length(a_tLS), 1, prob = a_tLS/sum(a_tLS))
+  b_state = sample(1:length(b_tLS), 1, prob = b_tLS/sum(a_tLS))
+  
+  #Separate time indices much be kept to maintain substitution independence between teams
+  a_T = 0
+  b_T = 0
+  game_duration = 60 * 48
+  interval_start = 0 # beginning of the current time interval 
+  
+  while (a_T < game_duration || b_T < game_duration) {
+    if (a_T <= b_T) {
+      #Advancing a_T
+      interval_start = a_T
+      
+      trans_prob = a_tMC[a_state, -a_state] / a_tLT[a_state]
+      
+      while (sum(trans_prob) == 0) {
+        trans_prob = a_tMC[-a_state, a_state] / a_tLT[a_state]
+        rate_prob = rexp(length(trans_prob), trans_prob)
+        rate_prob[is.na(rate_prob)] = max(rate_prob[!is.na(rate_prob)]) + 1
+        stateN = which.min(rate_prob)
+        if (stateN >= a_state) stateN = stateN + 1
+        a_state = stateN
+        trans_prob = a_tMC[a_state, -a_state] / a_tLT[a_state]
+      }
+      
+      rate_prob = rexp(length(trans_prob), trans_prob)
+      rate_prob[is.na(rate_prob)] = max(rate_prob[!is.na(rate_prob)]) + 1
+      if (a_T + min(rate_prob) > game_duration) {
+        a_LU_sim[a_state] = a_LU_sim[a_state] + (game_duration - a_T)
+        a_T = game_duration
+      } else {
+        a_T = a_T + min(rate_prob)
+        a_LU_sim[a_state] = a_LU_sim[a_state] + min(rate_prob)
+      }
+      stateN = which.min(rate_prob)
+      if (stateN >= a_state) stateN = stateN + 1
+      a_state = stateN
+    } else {
+      #Advancing b_T
+      interval_start = b_T
+      
+      trans_prob = b_tMC[b_state, -b_state] / b_tLT[b_state]
+      
+      while (sum(trans_prob) == 0) {
+        trans_prob = b_tMC[-b_state, b_state] / b_tLT[b_state]
+        rate_prob = rexp(length(trans_prob), trans_prob)
+        rate_prob[is.na(rate_prob)] = max(rate_prob[!is.na(rate_prob)]) + 1
+        stateN = which.min(rate_prob)
+        if (stateN >= b_state) stateN = stateN + 1
+        b_state = stateN
+        trans_prob = b_tMC[b_state, -b_state] / b_tLT[b_state]
+      }
+      
+      rate_prob = rexp(length(trans_prob), trans_prob)
+      rate_prob[is.na(rate_prob)] = max(rate_prob[!is.na(rate_prob)]) + 1
+      if (b_T + min(rate_prob) > game_duration) {
+        b_LU_sim[b_state] = b_LU_sim[b_state] + (game_duration - b_T)
+        b_T = game_duration
+      } else {
+        b_LU_sim[b_state] = b_LU_sim[b_state] + min(rate_prob)
+        b_T = b_T + min(rate_prob)
+      }
+      stateN = which.min(rate_prob)
+      if (stateN >= b_state) stateN = stateN + 1
+      b_state = stateN
+    }
+    
+    #Here we will increment scoring from what interval?
+    time_interval = min(a_T, b_T) - interval_start
+    
+    #Adjusting coefficients to account for linear restoring force
+    a_adjusted_coef = team_lineup_coef[[team_a]][a_state]
+    b_adjusted_coef = team_lineup_coef[[team_b]][b_state]
+    c = 0.01
+    score_differential = abs(a_plus_minus - b_plus_minus)
+    if (a_plus_minus > b_plus_minus) {
+      #a is winning
+      a_adjusted_coef = a_adjusted_coef - (score_differential * c)
+      b_adjusted_coef = b_adjusted_coef + (score_differential * c)
+    } else if (a_plus_minus < b_plus_minus) {
+      #b is winning
+      a_adjusted_coef = a_adjusted_coef + (score_differential * c)
+      b_adjusted_coef = b_adjusted_coef - (score_differential * c)
+    }
+    
+    a_plus_minus = a_plus_minus + (time_interval * a_adjusted_coef)
+    b_plus_minus = b_plus_minus + (time_interval * b_adjusted_coef)
+    
+  } #end while loop
+  
+  return(a_plus_minus - b_plus_minus)
+}
+
+#Example: simulating matchup between Chicago and Golden State 
+simulate_matchup("Chi", "GS")
